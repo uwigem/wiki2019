06198cb9314fe0882be44c525ca8ef59172a465e
Nitesh Chetry <chetry.nitesh@gmail.com>
2019-07-14 17:48:49 -0700

    Re-add semicolons after imports on ContentMapping.ts

--- src/components/ContentMapping/ContentMapping.ts
+++ src/components/ContentMapping/ContentMapping.ts
@@ -1,12 +1,12 @@
-import React from 'react'
-import { PlainText } from '../ContentWidgets/PlainText/PlainText'
-import { PlainTextEditor } from '../ContentWidgets/PlainText/PlainTextEditor'
-import { Stub, StubEditor } from '../ContentWidgets/_Stub/Stub'
-import { ContentSingularData } from '../_data/ContentSingularData'
-import { ExampleImage } from '../ContentWidgets/ExampleImage/ExampleImage'
-import { ExampleImageEditor } from '../ContentWidgets/ExampleImage/ExampleImageEditor'
-import { ImagesInRow } from '../ContentWidgets/ImagesInRow/ImagesInRow'
-import { ImagesInRowEditor } from '../ContentWidgets/ImagesInRow/ImagesInRowEditor'
+import React from 'react';
+import { PlainText } from '../ContentWidgets/PlainText/PlainText';
+import { PlainTextEditor } from '../ContentWidgets/PlainText/PlainTextEditor';
+import { Stub, StubEditor } from '../ContentWidgets/_Stub/Stub';
+import { ContentSingularData } from '../_data/ContentSingularData';
+import { ExampleImage } from '../ContentWidgets/ExampleImage/ExampleImage';
+import { ExampleImageEditor } from '../ContentWidgets/ExampleImage/ExampleImageEditor';
+import { ImagesInRow } from '../ContentWidgets/ImagesInRow/ImagesInRow';
+import { ImagesInRowEditor } from '../ContentWidgets/ImagesInRow/ImagesInRowEditor';
 
 export type WidgetEditorProps = {
     originalContent: ContentSingularData,
