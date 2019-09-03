65608573c53abb803e307ae89bf62d59191ae0cc
William Kwok <wkwok16@uw.edu>
2019-09-03 07:16:53 -0700

    Added information about deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,14 @@
 
 # PREREQUISITES: 
 # 	Install the iGEM-CLI globally: `npm install -g igem-cli`
+#	Set the environment variables inside .bashrc:
+#		export IGEM_USERNAME=username
+#		export IGEM_PASSWORD=password
+#		export IGEM_TEAM=Washington
+#		export IGEM_YEAR=2019
 #	Requires version: 1.0.2
-# TODO: Add stuff about username and pass aliases
+#
+# To run this file, go to the home directory of the repo and run `./scripts/deploy.sh`
 
 npm run build
 igem-cli -t JavaScript ./build/static/js/main.js
