3986842744aafb95574e6b705ea4b54be1755229
Nitesh Chetry <chetry.nitesh@gmail.com>
2019-10-06 11:55:45 -0700

    Use tabs instead of spaces, improve WidgetEditor.css naming

--- src/components/ContentEditor/WidgetEditor/WidgetEditor.tsx
+++ src/components/ContentEditor/WidgetEditor/WidgetEditor.tsx
@@ -8,11 +8,11 @@ import equal from 'deep-equal';
 import { EnvironmentContext } from '../../../contexts/EnvironmentContext/EnvironmentContext';
 
 type WidgetEditorProps = {
-    content: ContentSingularData | undefined,
-    contentHash: string,
-    currYear: number,
-    pageToEdit: string,
-    user: firebase.User | null
+	content: ContentSingularData | undefined,
+	contentHash: string,
+	currYear: number,
+	pageToEdit: string,
+	user: firebase.User | null
 }
 
 /**
@@ -31,88 +31,87 @@ type WidgetEditorProps = {
  *  - Make edithistory be its own class
  */
 export const WidgetEditor: React.FC<WidgetEditorProps> = ({ content, contentHash, currYear,
-    pageToEdit, user }) => {
-    const [editing, setEditing] = useState<boolean>(false);
-    const [editedContent, setEditedContent] = useState<ContentSingularData>({ ...content } as ContentSingularData);
-    const { firebase } = useContext(EnvironmentContext);
+	pageToEdit, user }) => {
+	const [editing, setEditing] = useState<boolean>(false);
+	const [editedContent, setEditedContent] = useState<ContentSingularData>({ ...content } as ContentSingularData);
+	const { firebase } = useContext(EnvironmentContext);
 
-    useEffect(() => {
-        if (!equal(content, editedContent)) {
-            setEditedContent({ ...content } as ContentSingularData);
-        }
-    }, [content])
+	useEffect(() => {
+		if (!equal(content, editedContent)) {
+			setEditedContent({ ...content } as ContentSingularData);
+		}
+	}, [content])
 
 
-    if (!content || !firebase) {
-        return <></>;
-    }
-    console.log(editedContent.type);
-    let ContentWidget = ContentMapping[editedContent.type].widget;
-    let ContentEditingWidget = ContentMapping[editedContent.type].editor;
-    return <div className="widget-editor">
-        {!editing && <>
-            <ContentWidget {...editedContent} />
-            <div>
-                <Button variant="contained" color="primary"
-                    onClick={() => setEditing(true)}>Edit</Button>
-            </div>
-        </>}
+	if (!content || !firebase) {
+		return <></>;
+	}
+	let ContentWidget = ContentMapping[editedContent.type].widget;
+	let ContentEditingWidget = ContentMapping[editedContent.type].editor;
+	return <div className="widget-editor">
+		{!editing && <>
+			<ContentWidget {...editedContent} />
+			<div>
+				<Button variant="contained" color="primary"
+					onClick={() => setEditing(true)}>Edit</Button>
+			</div>
+		</>}
 
-        {editing && <>
-            <div>
-                <div className="wiki-form">
-                    <form>
-                        <fieldset>
-                            <legend>Select a Widget</legend>
-                            <select id="widget" className="widget-type"
-                                value={editedContent.type}
-                                onChange={(e) => {
-                                    setEditedContentOnChange("type", e.target.value as string, editedContent, setEditedContent);
-                                }}>
-                                <option value="" disabled selected>-- Select a Widget --</option>
-                                {/* display Widget Categories and Widgets in dropdown, sorted alphabetically */}
-                                {Object.keys(WidgetCategories).map((category) => {
-                                    const categoryWidgets = Object.keys(ContentMapping).filter(widgetKey => ContentMapping[widgetKey].widgetCategory === category)
-                                    categoryWidgets.sort();
-                                    if (categoryWidgets.length > 0) {
-                                        return <optgroup label={category}>
-                                            {categoryWidgets.map((widgetKey) => {
-                                                return <option key={widgetKey} value={widgetKey}>
-                                                    {ContentMapping[widgetKey].displayName}
-                                                </option>
-                                            })}
-                                        </optgroup>
-                                    } else {
-                                        return <></>
-                                    }
-                                })}
-                            </select>
-                        </fieldset>
-                    </form>
-                </div>
-            </div>
-            <ContentEditingWidget editedContent={editedContent}
-                originalContent={content}
-                setEditedContentOnChange={(keyToChange: string, valueToChange: string) => {
-                    setEditedContentOnChange(keyToChange, valueToChange, editedContent, setEditedContent);
-                }} />
-            <div>
-                <Button variant="contained" color="primary"
-                    onClick={async () => {
-                        await firebase.database().ref(`${currYear}/ContentData/${pageToEdit}/content/${contentHash}`).set(editedContent);
-                        await firebase.database().ref(`${currYear}/EditHistory/${pageToEdit}/${contentHash}`).push({
-                            type: HistoryTypes.UPDATE,
-                            timestamp: firebase.database.ServerValue.TIMESTAMP,
-                            creator: (user && user.email) || "Unknown user",
-                            content: editedContent
-                        });
-                        setEditing(false);
-                    }}>
-                    Save
+		{editing && <>
+			<div>
+				<div className="widget-picker">
+					<form>
+						<fieldset>
+							<legend>Select a Widget</legend>
+							<select
+								value={editedContent.type}
+								onChange={(e) => {
+									setEditedContentOnChange("type", e.target.value as string, editedContent, setEditedContent);
+								}}>
+								<option value="" disabled selected>-- Select a Widget --</option>
+								{/* display Widget Categories and Widgets in dropdown, sorted alphabetically */}
+								{Object.keys(WidgetCategories).map((category) => {
+									const categoryWidgets = Object.keys(ContentMapping).filter(widgetKey => ContentMapping[widgetKey].widgetCategory === category)
+									categoryWidgets.sort();
+									if (categoryWidgets.length > 0) {
+										return <optgroup label={category}>
+											{categoryWidgets.map((widgetKey) => {
+												return <option key={widgetKey} value={widgetKey}>
+													{ContentMapping[widgetKey].displayName}
+												</option>
+											})}
+										</optgroup>
+									} else {
+										return <></>
+									}
+								})}
+							</select>
+						</fieldset>
+					</form>
+				</div>
+			</div>
+			<ContentEditingWidget editedContent={editedContent}
+				originalContent={content}
+				setEditedContentOnChange={(keyToChange: string, valueToChange: string) => {
+					setEditedContentOnChange(keyToChange, valueToChange, editedContent, setEditedContent);
+				}} />
+			<div>
+				<Button variant="contained" color="primary"
+					onClick={async () => {
+						await firebase.database().ref(`${currYear}/ContentData/${pageToEdit}/content/${contentHash}`).set(editedContent);
+						await firebase.database().ref(`${currYear}/EditHistory/${pageToEdit}/${contentHash}`).push({
+							type: HistoryTypes.UPDATE,
+							timestamp: firebase.database.ServerValue.TIMESTAMP,
+							creator: (user && user.email) || "Unknown user",
+							content: editedContent
+						});
+						setEditing(false);
+					}}>
+					Save
                 </Button>
-            </div>
-        </>}
-    </div>
+			</div>
+		</>}
+	</div>
 }
 
 
@@ -131,8 +130,8 @@ export const WidgetEditor: React.FC<WidgetEditorProps> = ({ content, contentHash
  * @param valueToChange value of the key value pair to update. Any type.
  */
 export const setEditedContentOnChange = (keyToChange: string, valueToChange: any,
-    editedContent: ContentSingularData,
-    setEditedContent: React.Dispatch<React.SetStateAction<ContentSingularData>>) => {
-    const updatedContent = { ...editedContent, [keyToChange]: valueToChange } as ContentSingularData;
-    setEditedContent(updatedContent);
+	editedContent: ContentSingularData,
+	setEditedContent: React.Dispatch<React.SetStateAction<ContentSingularData>>) => {
+	const updatedContent = { ...editedContent, [keyToChange]: valueToChange } as ContentSingularData;
+	setEditedContent(updatedContent);
 }
--- src/components/ContentMapping/ContentMapping.ts
+++ src/components/ContentMapping/ContentMapping.ts
@@ -18,84 +18,84 @@ import { SeparatorEditor } from '../ContentWidgets/Separator/SeparatorEditor';
 
 
 export type WidgetEditorProps = {
-    originalContent: ContentSingularData,
-    editedContent: ContentSingularData,
-    setEditedContentOnChange: (keyToChange: string, valueToChange: any) => void
+	originalContent: ContentSingularData,
+	editedContent: ContentSingularData,
+	setEditedContentOnChange: (keyToChange: string, valueToChange: any) => void
 }
 
 type ContentMappingType = {
-    [idx: string]: {
-        widget: React.FC<ContentSingularData>,
-        editor: React.FC<WidgetEditorProps>
-        displayName: string,
-        widgetCategory: WidgetCategories
-    }
+	[idx: string]: {
+		widget: React.FC<ContentSingularData>,
+		editor: React.FC<WidgetEditorProps>
+		displayName: string,
+		widgetCategory: WidgetCategories
+	}
 }
 
 export enum WidgetCategories {
-    Display = "Display",
-    Media = "Media",
-    Text = "Text"
+	Display = "Display",
+	Media = "Media",
+	Text = "Text"
 }
 
 export enum WidgetTypes {
-    PLAIN_TEXT = "PLAIN_TEXT",
-    STUB = "STUB",
-    EXAMPLE_IMAGE = "EXAMPLE_IMAGE",
-    TAB_VIEW = "TAB_VIEW",
-    SEPARATOR = "SEPARATOR",
-    IMAGES_IN_ROW = "IMAGES_IN_ROW",
-    PDF_VIEWER = "PDF_VIEWER",
-    VIDEO = "VIDEO"
+	PLAIN_TEXT = "PLAIN_TEXT",
+	STUB = "STUB",
+	EXAMPLE_IMAGE = "EXAMPLE_IMAGE",
+	TAB_VIEW = "TAB_VIEW",
+	SEPARATOR = "SEPARATOR",
+	IMAGES_IN_ROW = "IMAGES_IN_ROW",
+	PDF_VIEWER = "PDF_VIEWER",
+	VIDEO = "VIDEO"
 }
 
 export const ContentMapping: ContentMappingType = {
-    PLAIN_TEXT: {
-        widget: PlainText,
-        editor: PlainTextEditor,
-        displayName: "Plain Text",
-        widgetCategory: WidgetCategories.Text
-    },
-    STUB: {
-        widget: Stub,
-        editor: StubEditor,
-        displayName: "Stub",
-        widgetCategory: WidgetCategories.Display
-    },
-    EXAMPLE_IMAGE: {
-        widget: ExampleImage,
-        editor: ExampleImageEditor,
-        displayName: "Example Image",
-        widgetCategory: WidgetCategories.Media
-    },
-    TAB_VIEW: {
-        widget: TabView,
-        editor: TabViewEditor,
-        displayName: "Tab View",
-        widgetCategory: WidgetCategories.Display
-    },
-    IMAGES_IN_ROW: {
-        widget: ImagesInRow,
-        editor: ImagesInRowEditor,
-        displayName: "Images in Row",
-        widgetCategory: WidgetCategories.Media
-    },
-    SEPARATOR: {
-        widget: Separator,
-        editor: SeparatorEditor,
-        displayName: "Seperator",
-        widgetCategory: WidgetCategories.Display
-    },
-    PDF_VIEWER: {
-        widget: PdfViewer,
-        editor: PdfViewerEditor,
-        displayName: "PDF Viewer",
-        widgetCategory: WidgetCategories.Text
-    },
-    VIDEO: {
-        widget: Video,
-        editor: VideoEditor,
-        displayName: "Video",
-        widgetCategory: WidgetCategories.Media
-    },
+	PLAIN_TEXT: {
+		widget: PlainText,
+		editor: PlainTextEditor,
+		displayName: "Plain Text",
+		widgetCategory: WidgetCategories.Text
+	},
+	STUB: {
+		widget: Stub,
+		editor: StubEditor,
+		displayName: "Stub",
+		widgetCategory: WidgetCategories.Display
+	},
+	EXAMPLE_IMAGE: {
+		widget: ExampleImage,
+		editor: ExampleImageEditor,
+		displayName: "Example Image",
+		widgetCategory: WidgetCategories.Media
+	},
+	TAB_VIEW: {
+		widget: TabView,
+		editor: TabViewEditor,
+		displayName: "Tab View",
+		widgetCategory: WidgetCategories.Display
+	},
+	IMAGES_IN_ROW: {
+		widget: ImagesInRow,
+		editor: ImagesInRowEditor,
+		displayName: "Images in Row",
+		widgetCategory: WidgetCategories.Media
+	},
+	SEPARATOR: {
+		widget: Separator,
+		editor: SeparatorEditor,
+		displayName: "Seperator",
+		widgetCategory: WidgetCategories.Display
+	},
+	PDF_VIEWER: {
+		widget: PdfViewer,
+		editor: PdfViewerEditor,
+		displayName: "PDF Viewer",
+		widgetCategory: WidgetCategories.Text
+	},
+	VIDEO: {
+		widget: Video,
+		editor: VideoEditor,
+		displayName: "Video",
+		widgetCategory: WidgetCategories.Media
+	},
 }
